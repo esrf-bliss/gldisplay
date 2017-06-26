092617a5dc84d2070b03820e651023f989b94019
Aitor Henry <aitor@lid00limace.esrf.fr>
2017-06-26 17:42:49 +0200

    Cmake for GLDisplay third-party. test not working but still adding them.

--- test/test_gldisplay_thirdparty.cpp
+++ test/test_gldisplay_thirdparty.cpp
@@ -20,7 +20,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //###########################################################################
-#include "GLDisplay.h"
+#include "../include/GLDisplay.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
